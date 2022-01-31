9ce5688c0c3f072988e14a23dbb883703564dbf4
Md Nazmus Shakib <nazshakib01@gmail.com>
2022-01-31 22:43:22 +0600

    compressed img and made the site responsive

--- styles.js
+++ styles.js
@@ -18,3 +18,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+function hideNev() {
+  var element = document.getElementById("navbarTogglerDemo01");
+  element.classList.remove("show")
+}
